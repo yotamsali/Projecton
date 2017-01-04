7ffeb115b4c0330f84687bc1fc7fff2cd120ecf9
yotamsali <yotamsali@gmail.com>
2017-01-04 23:21:19 +0200

    Images from the camera with Nadav's car. Croped from video

--- ImageDetectTl.py
+++ ImageDetectTl.py
@@ -3,12 +3,6 @@ import numpy as np
 from scipy.misc import imresize
 from skimage import io, feature , color, exposure
 
-FACTOR = 0.2
-THRESHOLD = 0.43
-TEMP1 = 0.09
-TEMP2 = 0.06
-
-
 def templateMatcher (im, factorTemplate):
     """
     tlIm = io.imread('tl.png')
@@ -53,37 +47,79 @@ def toc():
     else:
         print ("Toc: start time not set")
 
+def tocReturn():
+    import time
+    if 'startTime_for_tictoc' in globals():
+        return time.time() - startTime_for_tictoc
+    else:
+        print ("Toc: start time not set")
+
 #********** SCRIPT *********************************************************
 
+def cameraTry(Total):
 
+    THRESHOLD1 = 0.65
+    THRESHOLD2 = 0.70
+    THRESHOLD3 = 0.65
 
+    FACTOR = 0.5 * Total
+    TEMP1 = 0.075 * Total
+    TEMP2 = 0.05 * Total
+    TEMP3 = 0.12 * Total
 
-camera = io.imread('tlData2.png')
-tic()
-camera = color.rgb2gray(camera)
-lstOrig = camera.shape
-width = lstOrig[1]
-height = lstOrig[0]
-newWidth = (int)(width * FACTOR)
-newHeight = (int)(height * FACTOR)
-print(newHeight)
-list = [newHeight, newWidth]
-arraySize = np.array(list)
-print(arraySize)
-print([height, width])
-tlIm = imresize(camera, arraySize)
-tlIm = exposure.equalize_adapthist(tlIm)
-toc()
-tic()
-map1 = templateMatcher(tlIm, TEMP1);
-map2 =  templateMatcher(tlIm,TEMP2);
-bin1 = map1 > THRESHOLD
-bin2 = map2 > THRESHOLD
-toc()
+    rowMin = (int)(0 * Total)
+    rowMax = (int)(250 * Total)
+    culMin = (int)(50 * Total)
+    culMax = (int)(750 * Total)
 
-f, arr = matplotlib.pyplot.subplots(1, 3)
-arr[0].imshow(tlIm, cmap='gray', interpolation='nearest')
-arr[1].imshow(bin1, cmap='gray', interpolation='nearest')
-arr[2].imshow(bin2, cmap='gray', interpolation='nearest')
-
+    camera = io.imread('tlData3.png')
+    tic()
+    camera = color.rgb2gray(camera)
+    lstOrig = camera.shape
+    width = lstOrig[1]
+    height = lstOrig[0]
+    newWidth = (int)(width * FACTOR)
+    newHeight = (int)(height * FACTOR)
+    list = [newHeight, newWidth]
+    arraySize = np.array(list)
+    tlIm = imresize(camera, arraySize)
+    tlIm = exposure.equalize_adapthist(tlIm)
+    tim1 = tocReturn();
+    tic()
+    tlIm = tlIm[rowMin:rowMax, culMin:culMax]
+    map1 = templateMatcher(tlIm, TEMP1);
+    map2 = templateMatcher(tlIm, TEMP2);
+    map3 = templateMatcher(tlIm, TEMP3)
+    bin1 = map1 > THRESHOLD1
+    bin2 = map2 > THRESHOLD2
+    bin3 = map3 > THRESHOLD3
+    """""
+    f, arr = matplotlib.pyplot.subplots(1, 3)
+    arr[0].imshow(tlIm, cmap='gray', interpolation='nearest')
+    arr[1].imshow(bin1, cmap='gray', interpolation='nearest')
+    arr[2].imshow(bin2, cmap='gray', interpolation='nearest')
+    matplotlib.pyplot.show()
+    """""
+    tim2 = tocReturn()
+    return [tim1, tim2]
+const = 17
+res = []
+for k in range(const):
+    res+=[0.2+k*0.05]
+print(res)
+timeTemplate = [0 for i in range(len(res))]
+timeNabaz = [0 for i in range(len(res))]
+ind = 0
+for elem in res:
+    vec = cameraTry(elem)
+    timeTemplate[ind] = (vec[1]/3)
+    timeNabaz[ind] = vec[0]
+    ind += 1
+print(timeTemplate)
+print(timeNabaz)
+matplotlib.pyplot.plot(res, timeTemplate)
 matplotlib.pyplot.show()
+matplotlib.pyplot.plot(res, timeNabaz)
+matplotlib.pyplot.show()
+
+
