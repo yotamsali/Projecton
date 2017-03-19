6955b7500f207e105602551c6b8cbfbc0e9f75c5
yotamsali <yotamsali@gmail.com>
2017-03-19 17:50:18 +0200

    Images from the camera with Nadav's car. Croped from video

---
+++ ImResize.py
@@ -0,0 +1,22 @@
+from scipy.misc import imresize
+from skimage import io
+
+
+def tic():
+    #Homemade version of matlab tic and toc functions
+    import time
+    global startTime_for_tictoc
+    startTime_for_tictoc = time.time()
+
+def toc():
+    import time
+    if 'startTime_for_tictoc' in globals():
+        print("Elapsed time is " + str(time.time() - startTime_for_tictoc) + " seconds.")
+    else:
+        print ("Toc: start time not set")
+
+camera = io.imread('goodim.jpg')
+tic()
+for i in range(1,2):
+    imresize(camera,(1000,500))
+toc()
