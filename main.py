e567a79ceb7037d025f12d11db8ca9600e07935c
gil <gilbo3210@gmail.com>
2017-03-21 13:41:59 +0200

    still need to add tracking
    unresolved:
    how do we check if TL is lost?

--- main.py
+++ main.py
@@ -9,16 +9,25 @@ path = 'try.avi'
 strm = Streamer(path)
 global carCntrl
 
-def trackMain (tlim, indX, indY, fullim):
-    # Here to implement green blinking (ירוק מהבהב)
-    color = getColor(tlim)
-    dist = getDistance(tlim, indX, indY, fullim)
-    DecisionMaker(color, dist)
+
+#tlim - traffic light image
+#indX, indY - coordinates of upper left corner of TL
+#fullim - complete camera image
+#exits when done tracking light
+#TODO insert CNN check every time period
+
+def trackMain (tl_im, indX, indY, fullim):
+    while(notLost): #TODO how to check if lost TL
+        #TODO Here to implement green blinking (ירוק מהבהב)
+        color = getColor(tl_im)
+        dist = getDistance(tl_im, indX, indY, fullim)
+        DecisionMaker(color, dist)
+
 
 def DecisionMaker(color, distance):
     if color == 'Green':
         carCntrl.Drive(50)
-    # Here to implement green blinking (ירוק מהבהב)
+    #TODO Here to implement green blinking (ירוק מהבהב)
     else:
         carCntrl.Break(distance)
 
@@ -33,7 +42,7 @@ def main():
             if (getDirection(cam[0]) == direc):
                 listOfOurTl += [cam]
         if len(listOfOurTl) > 1:
-            # check
+            # check TODO document what this function does
             tlChosen = [max(listOfOurTl, key=lambda p: p[0].shape[0] * p.shape[1])]
         elif len(listOfOurTl) == 1:
             tlChosen = listOfOurTl[0]
@@ -43,7 +52,3 @@ def main():
             trackMain(tlChosen[0], tlChosen[1], tlChosen[2], im)
         else:
             carCntrl.Drive(65)
-
-
-
-
