c98a4cf4290943ddec87fd3164c42b603aff91d8
yotamsali <yotamsali@gmail.com>
2017-03-22 22:22:29 +0200

    tuti code in carControl

--- carControl.py
+++ carControl.py
@@ -1,14 +1,35 @@
+import serial , time
 class carControl:
     def __init__(self):
         # directions are 'F' 'U' 'L' 'R'
+        self.carSerial = serial.Serial(port='COM14')
+        time.sleep(1)
         self.direction = 'F'
 
-    def Drive(spd, self):
-        if(spd):
-            return
+    def moveCar(command, self):
+        FORWARD = 1
+        REVERSE = 2
+        STOP = 0
+        if (command == STOP):
+            self.carSerial.write('s'.encode())
+        if (command == FORWARD):
+            self.carSerial.write('f'.encode())
+        if (command == REVERSE):
+            self.carSerial.write('r'.encode())
+
         return
-    def Break(dist, self):
+
+    def drive(self,spd):
+        if (spd > 0):
+            self.moveCar(1)
+        elif (spd < 0):
+            self.moveCar(2)
+        else:
+            self.moveCar(0)
         return
+
+a = carControl()
+a.moveCar(1)
 
 
 
