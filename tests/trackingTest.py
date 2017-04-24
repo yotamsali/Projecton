8d36e95f36c1d7d8ecd49a3b714a4797cce3570f
aviv <aviv0040@gmail.com>
2017-04-24 23:37:20 +0300

    updated tracking

--- tests/trackingTest.py
+++ tests/trackingTest.py
@@ -6,6 +6,7 @@ import cv2
 from matplotlib import pyplot as plt
 import streamer
 import matplotlib
+
 def getFirstImage(path):
     vidcap = cv2.VideoCapture(path)
     success,image = vidcap.read()
@@ -18,13 +19,13 @@ def getFirstImage(path):
       cv2.waitKey()
       cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
       count += 1
-    image = cv2.imread('frame0.jpg')[245:321, 891:917]
+    image = cv2.imread('frame0.jpg')[353:399, 1049:1064]
     plt.imshow(image)
     plt.show()
     cv2.waitKey()
     cv2.imwrite("trackingTestPic1.jpg", image)  # save frame as JPEG file
 
-path = 'ourCam25.3 16'
+
 """
 getFirstImage(path)
 
@@ -33,21 +34,35 @@ plt.imshow(im)
 plt.show()
 """
 
+
+path = '/home/aviv/PycharmProjects/Projecton/tests/photos/videos/ourCam22_3_13'
 ster = streamer.Streamer(path, 10)
 im = ster.getNext()
-up, down, left, right = 245, 321, 891, 917
+matplotlib.pyplot.imshow(im)
+matplotlib.pyplot.show()
+#location of the traffic light in the first frame
+up, down, left, right = 353,399, 1049,1064
 
 template = im[up:down, left:right]
 matplotlib.pyplot.imshow(template)
 matplotlib.pyplot.show()
+diff = [0,0]
+
 for i in range(900):
+
     pt2 = (up + template.shape[0], left + template.shape[1])
     print(i)
     cv2.rectangle(im, (left, up), (pt2[1], pt2[0]), (225, 0, 0))
     im = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
-    #cv2.imshow("grrr", im)
+    cv2.imshow("grrr", im)
     cv2.imwrite("frame%d.jpg"%i, im)
     im = ster.getNext()
+    im = ster.getNext()
+    im = ster.getNext()
 
-    template, (up,left) = Tracking.Track(im, template, (up,left))
-#"""
+    Tl, (up,left),template, diff  = Tracking.Track(im, template, (up,left), diff)
+    matplotlib.pyplot.imshow(Tl)
+    matplotlib.pyplot.show()
+    matplotlib.pyplot.imshow(im)
+    matplotlib.pyplot.show()
+
