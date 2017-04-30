096e5977d69f2d4fb73f53125a3b5843028e4562
yovelrom <yovelrom@gmail.com>
2017-04-30 04:43:34 +0300

    fixed the 'q' button, now goes over the folder in order. soon to be tracked.

--- cnn training/label_Images.py
+++ cnn training/label_Images.py
@@ -4,13 +4,15 @@ import cv2
 from scipy import misc as m
 import os
 import random
+import Tracking
 
 # initialize the list of reference points and boolean indicating
 # whether cropping is being performed or not
 refPt = []
 cropping = False
 NUMBER_OF_IMAGES = 20 ;
-
+X = 0
+Y = 1
 
 def getIndexes():
     if(not os.path.exists(os.getcwd()+"/counter.txt")):
@@ -86,7 +88,31 @@ def getNewImageCoo(cord):
         else:
             return [(cord[1][0], cord[1][1]), (cord[0][0], cord[0][1])]
 
-
+def save_twenty_random(cooAr, image, ):
+    tl_width = abs(cooAr[0][0] - cooAr[1][0])
+    tl_height = abs(cooAr[0][1] - cooAr[1][1])
+    tl_up = cooAr[0][1]
+    tl_down = cooAr[1][1]
+    tl_right = cooAr[1][0]
+    tl_left = cooAr[0][0]
+    image_height, image_width, useless = image.shape
+    for i in range(NUMBER_OF_IMAGES):
+        random_right = random.randint(0, tl_width / 2)
+        random_left = random.randint(0, tl_width / 2)
+        random_up = random.randint(0, tl_height / 2)
+        random_down = random.randint(0, tl_height / 2)
+        if (tl_right + random_right >= image_width):
+            random_right = image_width - tl_right
+        if (tl_left - random_left <= 0):
+            random_left = tl_left
+        if (tl_up - random_up <= 0):
+            random_up = tl_up
+        if (tl_down + random_down >= image_height):
+            random_down = image_height - tl_down
+        roi = clone[tl_up - random_up:tl_down + random_down,tl_left - random_left:tl_right + random_right]
+        goodpath = "posIndex:" + str(i) + "-u_" + str(random_up) + "_d_" + str(random_down) + "_l_" + str(
+            random_left) + "_r_" + str(random_right) + "_number:" + str(posCounter) + ".jpg"
+        m.imsave(os.path.join("pos", goodpath), roi[..., ::-1])
 
 
 
@@ -101,7 +127,7 @@ print ("How to use the program?\nwrite the path of the folder where the images a
 path = raw_input("Enter the path of the pictures folder\n")
 print("Searching images in folder: "+path)
 pictures = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
-pictures.sort()
+pictures.sort(key = lambda x: int(x[:-4]))
 to_break = False # a boolean to break the loop with 'q'
 for pictureName in pictures:
     # load the image, clone it, and setup the mouse callback function
@@ -154,6 +180,7 @@ for pictureName in pictures:
 
         elif key == ord("b"):
             cooAr = getNewImageCoo(refPt)
+            print(cooAr)
             roi = clone[cooAr[0][1]:cooAr[1][1], cooAr[0][0]:cooAr[1][0]]
             badpath = str(negCounter) + ".jpg"
             m.imsave(os.path.join("neg", badpath), roi[..., ::-1])
@@ -167,6 +194,58 @@ for pictureName in pictures:
         elif key == ord("q"):
             to_break = True
             break
+
+        #if you want to track
+        elif key == ord('t'):
+            cooAr = getNewImageCoo(refPt)
+            #these are just the lines from 'g', copied. saves the firest image
+            save_twenty_random(cooAr, image)
+            diff = [0,0]
+            template = image[cooAr[0][Y]:cooAr[1][Y], cooAr[0][X]:cooAr[1][X]]
+            xy = (cooAr[0][Y], cooAr[0][X])
+            base = pictures.index(pictureName)
+            counter = 1
+            #saves the picture so it will be possible to return to it
+            oldImage = clone.copy()
+            while True:
+                #reads the next image in the list
+                image = cv2.imread(path+"/"+pictures[base + counter])
+                #delegates to tracking
+                useless, xy, template, diff = Tracking.Track(image, template, xy, diff)
+                #clones so the rectangle will not be saved
+                imClone = image[:].copy()
+                cv2.rectangle(imClone, (xy[1], xy[0]), (xy[1] + template.shape[1], xy[0] + template.shape[0]), (0, 255, 0), 2)
+                cv2.imshow("image", imClone)
+                key = cv2.waitKey(100) & 0xFF
+                #stops the process and returns to the first image, to choose new stoplight
+                if key == ord('s'):
+                    image = oldImage
+                    cv2.imshow("image", image)
+                    break
+                #pauses the process to let the user decide on new borders
+                elif key == ord('p'):
+                    cv2.imshow("image", image)
+                    cv2.setMouseCallback("image", click_and_crop)
+                    while True:
+                        key = cv2.waitKey(1) & 0xFF
+                        #press c after the new border is decided
+                        if key == ord('c'):
+                            break
+                    cooAr = getNewImageCoo(refPt)
+                    save_twenty_random(cooAr, image)
+                    template = image[cooAr[0][Y]:cooAr[1][Y], cooAr[0][X]:cooAr[1][X]]
+                    cooArNew = [(xy[X], xy[Y]), (xy[X] + template.shape[X], xy[X] + template.shape[Y])]
+                    xy = (cooAr[0][Y], cooAr[0][X])
+
+                #just the usual tracking
+                else:
+                    cooArNew = [(xy[Y],xy[X]), (xy[Y] + template.shape[Y], xy[X] + template.shape[X])]
+                    save_twenty_random(cooArNew, image)
+                counter += 1
+
+
+
+
     if to_break:
         break
 
